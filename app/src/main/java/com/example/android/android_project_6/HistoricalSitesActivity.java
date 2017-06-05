14a67dceeac2d39401e11c509fa8c726977cbdc9
ElKebaboKing <ranielmaalouf@gmail.com>
2017-06-05 09:27:55 +0200

    Add activities to manifest, and create java class files for them.

---
+++ app/src/main/java/com/example/android/android_project_6/HistoricalSitesActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.android_project_6;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by es013620 on 2017-06-05.
+ */
+
+public class HistoricalSitesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
