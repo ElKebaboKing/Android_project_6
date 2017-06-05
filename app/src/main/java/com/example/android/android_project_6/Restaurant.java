e81f59426de3560f912cf7db233b44abc17a5657
ElKebaboKing <ranielmaalouf@gmail.com>
2017-06-05 10:24:33 +0200

    Create Restaurant object class.

---
+++ app/src/main/java/com/example/android/android_project_6/Restaurant.java
@@ -0,0 +1,22 @@
+package com.example.android.android_project_6;
+
+/**
+ * Created by es013620 on 2017-06-05.
+ */
+
+public class Restaurant {
+
+    String mName;
+    Location mLocation;
+
+    public Restaurant(String name, String adress)
+    {
+        mName = name;
+        mLocation = new Location(adress);
+    }
+
+    public String getLocation()
+    {
+        return mLocation.getLocationByAdress();
+    }
+}
